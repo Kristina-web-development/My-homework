22cd81835ded3d07464932debcf1b11ae77ea183
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2023-05-07 19:10:46 +0300

    js start

---
+++ 2 - JavaScript/hw.md
@@ -0,0 +1,2 @@
+1. Create a function to calculate the volume of a room.
+2. Create a function that gets the sizes of the box and returns how many boxes that room can include.
---
+++ 2 - JavaScript/index.js
@@ -0,0 +1,89 @@
+// console.log("hello")
+
+// alert("I'm here") //return true
+// const userOk = confirm("Are you okey") //true / false
+// console.log(userOk)
+
+// const nameUser = prompt("Enter your name")//By default return string
+// console.log(nameUser)
+
+// const num = +prompt("Enter number") //return a number - otherwise NaN = Not a Number
+// const num2 = Number(prompt("Enter number")) //return a number - otherwise NaN = Not a Number
+// console.log(num2)
+
+// ----- Types of variebale -------
+// NaN = Not a Number
+// string = "text",
+// number = 123
+// undefined - not found
+// null - empty
+// boolean = true/false
+// Object
+// Array
+// Date
+
+
+//------- Const / Let -------
+// const - can not change in the future
+// let - can change in the future
+
+// const hello = "Hello"
+// console.log(hello)
+
+// hello = "Hi"
+// console.log(hello)
+
+// let world = "World"
+// console.log(world)
+
+// world = "Sky"
+// console.log(world)
+
+//------- Operators ----------
+// const x = 5
+// const y = 3
+
+// const a = x + y
+// const a1 = x * y
+// const a2 = x - y
+// const a3 = x / y
+// const a4 = x % y
+// const a5 = x ** y
+// const a6 = x++
+// const a7 = x--
+
+// let z = 5
+
+// z += 1
+// z -= 1
+// z *= 1
+// z /= 1
+// z %= 1
+// z **= 1
+
+//---- Function ---
+/* Regular */
+function fun(paramets, paramets1) { //Regular function can return "this"
+    return paramets + paramets1
+}
+
+/* Anonymous */
+(par) => {
+
+}
+
+/* Arrow function */
+const fun1 = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
