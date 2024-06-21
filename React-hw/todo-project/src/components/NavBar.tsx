17282f038a677f29d098719c3a237f7b49dbb00e
Kristina-web-development <99411070+Kristina-web-development@users.noreply.github.com>
2024-06-21 18:55:21 +0300

    new project

---
+++ React-hw/todo-project/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import iconSun from "../assets/images/icon-sun.svg"
+const NavBar = () => {
+  return (
+   <nav>
+       <h1>TODO</h1>
+       <img src={iconSun} alt="Icon Sun" width="26" height="26"/>
+  </nav>
+
+    
+    
+  )
+}
+
+export default NavBar
